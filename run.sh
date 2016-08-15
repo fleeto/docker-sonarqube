b1aa50b8db31b8bef906688fff21e88f7944b2f5
fleeto <git@fleeto.us>
2016-08-15 12:27:10 +0800

    jar  path

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ if [ "${1:0:1}" != '-' ]; then
   exec "$@"
 fi
 
-exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
+exec java -jar $SONARQUBE_HOME/sonarqube/lib/sonar-application-$SONAR_VERSION.jar \
   -Dsonar.log.console=true \
   -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
   -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
@@ -14,6 +14,6 @@ exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
   -Dsonar.http.proxyHost="$SONARQUBE_HTTP_PROXY_HOST" \
   -Dsonar.http.proxyPort="$SONARQUBE_HTTP_PROXY_PORT" \
   -Dsonar.https.proxyHost="$SONARQUBE_HTTPS_PROXY_HOST" \
-  -Dsonar.https.proxyPort="$SONARQUBE_HTTPS_PROXY_PORT" \  
+  -Dsonar.https.proxyPort="$SONARQUBE_HTTPS_PROXY_PORT" \
   -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Djava.security.egd=file:/dev/./urandom" \
   "$@"
