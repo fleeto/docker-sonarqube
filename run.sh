a16e4fbed63c1470f132ed3edf81f9b2ea28d44a
fleeto <git@fleeto.us>
2016-08-15 13:17:27 +0800

    Add proxy lines in run.sh

--- run.sh
+++ run.sh
@@ -6,14 +6,25 @@ if [ "${1:0:1}" != '-' ]; then
   exec "$@"
 fi
 
+CONF="$SONARQUBE_HOME/conf/sonar.properties"
+
+if [ -n "$SONARQUBE_HTTP_PROXY_HOST" ]; then
+  echo "http.proxyHost=$SONARQUBE_HTTP_PROXY_HOST\n" >> $CONF
+fi
+if [ -n "$SONARQUBE_HTTP_PROXY_PORT" ]; then
+  echo "http.proxyPort=$SONARQUBE_HTTP_PROXY_PORT\n" >> $CONF
+fi
+if [ -n "$SONARQUBE_HTTPS_PROXY_HOST" ]; then
+  echo "https.proxyHost=$SONARQUBE_HTTPS_PROXY_HOST\n" >> $CONF
+fi
+if [ -n "$SONARQUBE_HTTPS_PROXY_PORT" ]; then
+  echo "https.proxyPort=$SONARQUBE_HTTPS_PROXY_PORT\n" >> $CONF
+fi
+
 exec java -jar $SONARQUBE_HOME/lib/sonar-application-$SONAR_VERSION.jar \
   -Dsonar.log.console=true \
   -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
   -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
   -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
-  -Dsonar.http.proxyHost="$SONARQUBE_HTTP_PROXY_HOST" \
-  -Dsonar.http.proxyPort="$SONARQUBE_HTTP_PROXY_PORT" \
-  -Dsonar.https.proxyHost="$SONARQUBE_HTTPS_PROXY_HOST" \
-  -Dsonar.https.proxyPort="$SONARQUBE_HTTPS_PROXY_PORT" \
   -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Djava.security.egd=file:/dev/./urandom" \
   "$@"
